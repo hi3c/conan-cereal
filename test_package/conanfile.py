5d39818501ac3bfb486902bca8a50f810cfa32cd
Tobias Hieta <tobias@plex.tv>
2017-07-02 19:34:33 +0200

    Update test_package version as well

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,7 +8,7 @@ username = os.getenv("CONAN_USERNAME", "hi3c")
 
 class CerealTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "cereal/7a58c34@%s/%s" % (username, channel)
+    requires = "cereal/67f742f@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
